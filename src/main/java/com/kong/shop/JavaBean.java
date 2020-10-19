239948f5ab41913f584e59c2186307c194bf9cb8
kong <1069750214@qq.com>
2020-10-19 18:38:09 +0800

    vs Code提交

--- src/main/java/com/kong/shop/JavaBean.java
+++ src/main/java/com/kong/shop/JavaBean.java
@@ -3,7 +3,8 @@ package com.kong.shop;
 
 public class JavaBean {
     public static void main(String[] args) {
-        System.out.println(111);
+        System.out.println(111222222);
+        
     }
 
 
