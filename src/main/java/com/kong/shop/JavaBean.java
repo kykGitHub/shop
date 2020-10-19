225f0fb5cf99ccef6f27dc2fab2dc7a2385c3a84
kong <1069750214@qq.com>
2020-10-19 18:39:51 +0800

    VS  提交

--- src/main/java/com/kong/shop/JavaBean.java
+++ src/main/java/com/kong/shop/JavaBean.java
@@ -4,6 +4,13 @@ package com.kong.shop;
 public class JavaBean {
     public static void main(String[] args) {
         System.out.println(111222222);
+
+  System.out.println(111222222);
+    System.out.println(111222222);
+      System.out.println(111222222);
+        System.out.println(111222222);
+
+
         
     }
 
